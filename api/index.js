b8570ae042ece65f2640e0e8f2beda1b44071d12
ironminddr <ddrdaniel56@gmail.com>
2023-07-09 13:34:08 +0200

    comment code to see if vercel deployment are due to this

--- api/index.js
+++ api/index.js
@@ -38,10 +38,10 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   next(createError(404));
 });
-
+*/
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
